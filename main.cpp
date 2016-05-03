d20be863e2ff86c97d655c63728de22aa150b40b
C-Kimber <kimber.cli2703@CSiMac2-4.local>
2016-05-03 09:55:54 -0600

    added main
    dud i did th stff.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*//Self-Help program
+Contributors: Ainsley, Kimber, Johnathon, 
+//*/
+
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	int snt = 0;
+	
+	while(snt == 0)
+	{
+			
+		
+		
+		
+		
+	}
+	return 0;
+}
