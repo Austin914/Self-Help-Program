f6cc603c708ee728c4b3b5dac507aacf603276b2
Ainsley Hopkins <ainsley.hopkins@suusuccess.org>
2016-05-09 10:01:03 -0600

    Added some questions for Testing
    Added some questions for testing of the sequential access file if they were needed.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 /*//Self-Help program
-Contributors: Ainsley, Kimber, Johnathon, 
-//*/
+ Contributors: Ainsley, Kimber, Johnathon,
+ //*/
 
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 int main() {
-	int snt = 0;
-	
-	while(snt == 0)
-	{
-			
-		
-		
-		
-		
-	}
-	return 0;
+    int snt = 0;
+    string name = "";
+    string age = "";
+    string mail = "";
+    string pNum = "";
+    string address = "";
+    
+    
+    while(snt == 0)
+    { //Get the basic information from the user
+        
+        cout << "What's your name? (First Last)";
+        cin.ignore(100, ' ');
+        cin >> name;
+        cout << "How old are you? ";
+        cin >> age;
+        cout << "What's your email? ";
+        cin >> mail;
+        cout << "What is your phone number? ";
+        cin.ignore(100, ' ');
+        cin >> pNum;
+        cout << "What is your address? (Street, City, Zipcode) ";
+        cin.ignore(100, ' ');
+        cin >> address;
+
+        cout << "Enter a 0 to continue or another number to stop: ";
+        cin >> snt;
+    }
+    return 0;
 }
